a49ed10fbbb347a8627fb6e0b3f22e73950afb56
Jisuel <jesuel1@outlook.com>
2019-12-06 19:30:49 -0400

    Agregando Lista de socios.

---
+++ FinalWEB/FinalWEB/Controllers/SociosController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using FinalWEB.Models;
+
+namespace FinalWEB.Controllers
+{
+    public class SociosController : Controller
+    {
+        private DBModels db = new DBModels();
+
+        // GET: Socios
+        public ActionResult Index()
+        {
+            return View(db.Socios.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult View(int id)
+        {
+            Socios foto = new Socios();
+
+            using (DBModels db = new DBModels())
+            {
+                foto = db.Socios.Where(x => x.Id == id).FirstOrDefault();
+            }
+            return View("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Add(Socios image)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(image.ImageFile.FileName);
+            string extension = Path.GetExtension(image.ImageFile.FileName);
+            fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+            image.Foto = "~/Image/" + fileName;
+            fileName = Path.Combine(Server.MapPath("~/Image"), fileName);
+            image.ImageFile.SaveAs(fileName);
+            using (DBModels db = new DBModels())
+            {
+                db.Socios.Add(image);
+                db.SaveChanges();
+            }
+            ModelState.Clear();
+            return RedirectToAction("Index");
+        }
+
+        // GET: Socios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Socios socios = db.Socios.Find(id);
+            if (socios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(socios);
+        }
+
+        // GET: Socios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Socios/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nombre,Apellido,Cedula,Foto,Direccion,Telefono,Sexo,Edad,Fecha_Nacimiento,Afiliados,Membresia,Lugar_Trabajo,Direc_Oficina,Tel_Oficina,Estatus,Fecha_Ingreso,Fecha_Salida")] Socios socios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Socios.Add(socios);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(socios);
+        }
+
+        // GET: Socios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Socios socios = db.Socios.Find(id);
+            if (socios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(socios);
+        }
+
+        // POST: Socios/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nombre,Apellido,Cedula,Foto,Direccion,Telefono,Sexo,Edad,Fecha_Nacimiento,Afiliados,Membresia,Lugar_Trabajo,Direc_Oficina,Tel_Oficina,Estatus,Fecha_Ingreso,Fecha_Salida")] Socios socios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(socios).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(socios);
+        }
+
+        // GET: Socios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Socios socios = db.Socios.Find(id);
+            if (socios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(socios);
+        }
+
+        // POST: Socios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Socios socios = db.Socios.Find(id);
+            db.Socios.Remove(socios);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /*public ActionResult GetImage(int id)
+        {
+            Socios image = db.Socios.Find(id);
+            byte[] byteImage = image.Foto;
+
+            MemoryStream memoryStream = new MemoryStream(byteImage);
+            Image imagen = Image.FromStream(memoryStream);
+
+            memoryStream = new MemoryStream();
+            imagen.Save(memoryStream, ImageFormat.Jpeg);
+            memoryStream.Position = 0;
+
+            return File(memoryStream, "image/jpg");
+        }*/
+    }
+}
